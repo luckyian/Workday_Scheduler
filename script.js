8e99a1d1bd4a3f4c8495cf204eef50b85890747d
Ian Wren <ian.wren81@gmail.com>
2020-10-12 12:48:26 -0700

    Added the date to the top of the index page using moment.js

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
+var date = document.querySelector("#currentDay");
+
+date.textContent = moment().format("MMM Do YYYY");
+
+moment().hour(Number);
+
 $(document).ready(function() {
 let buttonEl = document.querySelector(".btn");
 
-$(".btn").on("click", function(){
-
+$(buttonEl).on("click", function(event){
+    event.preventDefault();
+    var setText = document.querySelector("#inlineFormInputGroup").value;
     
+    localStorage.setItem("setText", setText);
+
 } )
 });
+
