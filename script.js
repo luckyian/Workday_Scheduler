9ad0547d360689b718c733953ae2dca2969d0a6e
Ian Wren <ian.wren81@gmail.com>
2020-10-13 17:59:51 -0700

    Added functions to check current time using moment.js and use style.css to color html page according to the  readme instructions.

--- script.js
+++ script.js
@@ -4,6 +4,29 @@ date.textContent = moment().format("MMM Do YYYY");
 
 moment().hour(Number);
 
+var current = moment().hour(); // Number
+console.log(current);
+
+$(".form-row").each(function() {
+var blockHour = parseInt($(this).find(".hour").attr("id"))
+
+console.log($(this))
+if(blockHour < current) {
+$(this).parent().find(".form-row").addClass("past")
+
+}
+else if(blockHour === current) {
+    $(this).removeClass("past")
+    $(this).addClass("present")
+}
+else {
+    $(this).removeClass("past")
+    $(this).removeClass("present")
+    $(this).addClass("future")
+}
+
+});
+
 $(document).ready(function() {
 let buttonEl = document.querySelector(".btn");
 
