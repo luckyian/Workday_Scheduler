75dc5bc35a3907c0de77283cb43a3867d4b2a823
Ian Wren <ian.wren81@gmail.com>
2020-10-22 16:25:58 -0700

    Fixed local storage issues, fixed timestamp color issues, and cleaned up code.

--- script.js
+++ script.js
@@ -1,41 +1,49 @@
-var date = document.querySelector("#currentDay");
+$(document).ready(function () {
+let date = document.querySelector("#currentDay");
 
 date.textContent = moment().format("MMM Do YYYY");
 
 moment().hour(Number);
 
-var current = moment().hour(); // Number
+let current = moment().hour();
 console.log(current);
 
-$(".form-row").each(function() {
-var blockHour = parseInt($(this).find(".hour").attr("id"))
+$(".time-block").each(function () {
+    let blockHour = parseInt($(this).parents('.row').find('.hour').attr("id"));
 
-console.log($(this))
-if(blockHour < current) {
-$(this).parent().find(".form-row").addClass("past")
+    $(this).parents(".row").find(".time-block").val(localStorage.getItem(blockHour));
+    if (blockHour < current) {
+        $(this).parents(".row").find(".time-block").addClass("past")
 
-}
-else if(blockHour === current) {
-    $(this).removeClass("past")
-    $(this).addClass("present")
-}
-else {
-    $(this).removeClass("past")
-    $(this).removeClass("present")
-    $(this).addClass("future")
-}
+    }
+    else if (blockHour === current) {
+        $(this).removeClass("past")
+        $(this).addClass("present")
+    }
+    else {
+        $(this).removeClass("past")
+        $(this).removeClass("present")
+        $(this).addClass("future")
+    }
 
 });
 
-$(document).ready(function() {
-let buttonEl = document.querySelector(".btn");
 
-$(buttonEl).on("click", function(event){
-    event.preventDefault();
-    var setText = document.querySelector("#inlineFormInputGroup").value;
-    
-    localStorage.setItem("setText", setText);
+    let buttonEl = document.querySelector(".btn");
 
-} )
+   
+
+    $(".btn").on("click", function (event) {
+        
+        event.preventDefault();
+        
+        var time = $(this).attr("id");
+        console.log(time);
+        let setText = $(this).parents('.row').find('.time-block').val();
+        console.log(setText);
+        localStorage.setItem(time, setText);
+        
+
+    })
 });
 
